af83c14a51771a15b1122319223f9a66c574e032
JUNGYUMI <dbal7749@naver.com>
2020-06-26 17:13:26 +0900

    NAER 연습

--- NAVER/js/rolling.js
+++ NAVER/js/rolling.js
@@ -1,5 +1,5 @@
 $(function(){
-    setInterval(function(){
+    var newsRolling=setInterval(function(){
         $('.box-rolling-news').first().animate({'margin-top':'-24px'},700,function(){
             $(this).detach().appendTo('.box-rolling-newslist').css('margin-top','0px').removeAttr('style');
             /*
@@ -7,4 +7,19 @@ $(function(){
             $(A). prependTo(B) : B요소 안의 제일 앞에 A를 html코드로 추가해라 */
         });
     },2000)
+    var cardRollingNum = cardRolling();
+    $('.box-right2').hover(function(){
+        //마우스가 요소 안으로 들어갈 때
+        clearInterval(cardRollingNum);
+    },function(){
+        //마우스가 요소 밖으로 나갈 때
+        cardRollingNum = cardRolling();
+    })    
 })
+function cardRolling(){
+    return setInterval(function(){
+        $('.box-card-nav> .card').first().animate({'margin-left':'-281'},1000,function(){
+            $(this).detach().appendTo('.box-card-nav').removeAttr('style');
+        })
+    },3000)
+}
