e374091062323f48ad10c8e4a54b6dc8428aa2fc
JUNGYUMI <dbal7749@naver.com>
2020-06-29 17:01:38 +0900

    NAVER 선생님ver

--- NAVER/js/naver copy.js
+++ NAVER/js/naver copy.js
@@ -243,11 +243,54 @@ $(function(){
 		if($('.box-theme-wrap .btn-tab').last().attr('aria-selected') == 'true'){
 			$('.box-theme-wrap .btn-next').addClass('display-none');
 		}
-	}
-        function initMenu(){
-            $('.box-service-menu.display').addClass('display-none');
+    }
+    // 오른쪽 3번째 컨텐츠에서 상품/쇼핑몰/MEN이 선택되면,
+    // 선택된 내용에 맞는 box-shop-body가 보이도록하는 함수
+    function shopView(){
+        var target = $('.box-shop-header .tab[aria-selected=true]').attr('data-target');
+        $('.box-shop-body div').addClass('display-none');
+        $('.box-shop-body .' +target).removeClass('display-none');
+    }
+    shopView();
+    tabRandom();
+    function tabRandom(){
+        var arr = [];
+        $('.box-shop-middle .box-mall .link-mall').removeClass('random');
+        for( ; arr.length < 4;){
+            var r = getRandom(1, 12);
+            if(arr.indexOf(r) >= -0){              
+                continue;
+            }
+            arr.push(r)
+            if(r <= 6){
+                $('.box-shop-middle .box-mall').eq(0).find('.link-mall').eq(r-1).addClass('random');
+            }else{
+                $('.box-shop-middle .box-mall').eq(1).find('.link-mall').eq(r-7).addClass('random');
+            }
+        }
+    }
+    function getRandom(min, max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    $('.box-shop-header .tab').click(function(e){
+        e.preventDefault();
+        $('.box-shop-header .tab').attr('aria-selected','false');
+        $(this).attr('aria-selected','true');
+        shopView();
+        tabRandom();
+        var target = $(this).attr('data-target');
+        if(target == 'mall'){
+            $('.box-shop-middle').addClass('display-none');
+        }else{
+            $('.box-shop-middle').removeClass('display-none');
         }
     })
+    shopView();
+
+    function initMenu(){
+        $('.box-service-menu.display').addClass('display-none');
+    }
+})
     
 
 
